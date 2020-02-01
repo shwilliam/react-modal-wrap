a8c8130e974fb9b78925b6c5102e7c37af83505b
William L <w-lindvall@outlook.com>
2020-02-01 02:22:56 -0800

    delete ModalTrigger component

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import Modal from './ModalContainer'
 import ModalWrapper from './ModalContextProvider'
-import ModalTrigger from './ModalTrigger'
 import {useModal} from './hooks'
 
 export default Modal
-export {Modal, ModalWrapper, ModalTrigger, useModal}
+export {Modal, ModalWrapper, useModal}
